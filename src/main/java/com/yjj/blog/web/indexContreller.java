49987639b4bed3439400b6394695ae700d76e9fb
henryorpeter <1561784670@qq.com>
2020-10-13 23:36:20 +0800

    404、500、error错误页面的美化

--- src/main/java/com/yjj/blog/web/indexContreller.java
+++ src/main/java/com/yjj/blog/web/indexContreller.java
@@ -2,15 +2,17 @@ package com.yjj.blog.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class indexContreller {
 
-    @GetMapping("/{id}/{name}")
-    public String index(@PathVariable Integer id,@PathVariable String name) {
-        System.out.println("--------index--------");
-        int i=7/0;
+    @GetMapping("/")
+    public String index() {
         return "index";
+    }
+
+    @GetMapping("/blogs")
+    public String blogs() {
+        return "blogs";
     }
 }
